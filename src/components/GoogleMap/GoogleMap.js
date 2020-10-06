cdc01dc9c45a618915264a082caf869fd5edeb13
piaspro <golamrahman09@gmail.com>
2020-10-06 13:38:09 +0600

    google map location fixed

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,54 +1,45 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Marker } from 'google-maps-react';
+import { useParams } from 'react-router-dom';
  
 const SimpleMap = () => {
 
   const locations = [
     {
-      name: "Location 1",
+      name: "sundorban",
+      id: 2,
       location: { 
-        lat: 41.3954,
-        lng: 2.162 
+        lat: 21.9497,
+        lng: 89.1833 
       },
     },
     {
-      name: "Location 2",
+      name: "coxsbazar",
+      id: 0,
       location: { 
-        lat: 41.3917,
-        lng: 2.1649
-      },
+        lat: 21.4272, 
+        lng: 92.0058}
     },
     {
-      name: "Location 3",
+      name: "sreemongol",
+      id: 1,
       location: { 
-        lat: 41.3773,
-        lng: 2.1585
-      },
-    },
-    {
-      name: "Location 4",
-      location: { 
-        lat: 41.3797,
-        lng: 2.1682
-      },
-    },
-    {
-      name: "Location 5",
-      location: { 
-        lat: 41.4055,
-        lng: 2.1915
+        lat: 24.3065,
+        lng: 91.7296
       },
     }
   ];
- 
+  let { key } = useParams();
+  const findLocation = locations.find(element => element.id == key);
+  console.log(findLocation.location)
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '600px', width: '400px', borderRadius: '20px', paddingTop: '65px', paddingLeft: '50px'  }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAarM6laJv9yjXjXPMPfK--I-kFYmMnZ4s' }}
-          defaultCenter={{ lat: 23.450001, lng: 91.199997}}
-          defaultZoom={10}
+          defaultCenter={findLocation.location}
+          defaultZoom={12}
         >
         <Marker position={{ lat: 23.450001, lng: 91.199997}} />
         </GoogleMapReact>
